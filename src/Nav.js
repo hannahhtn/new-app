cfb1a22aae28c8804243374dfa6b5967f325170f
Hannah <hannahhtn97@gmail.com>
2024-01-10 23:00:10 -0800

    created empty list render

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,17 @@
-import { Heading } from "@chakra-ui/react";
+/** @format */
+
+import { Button, HStack, Heading } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 
 const Nav = ({ title }) => {
-    return (
-        <Heading align='center' size="xl" p="1rem">
-          {title}
-        </Heading>
-    );
-}
- 
+  return (
+    <HStack px="1rem" py="1.5rem" direction="row" justifyContent="space-between">
+      <Heading align="center" size="xl">
+        {title}
+      </Heading>
+      <Button p="0.5rem" colorScheme="teal" variant="outline" as="b">Add Gift</Button>
+    </HStack>
+  );
+};
+
 export default Nav;
